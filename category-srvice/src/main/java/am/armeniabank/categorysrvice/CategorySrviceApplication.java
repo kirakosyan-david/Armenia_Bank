e7e05306abd7c456231a98b98afdf462487a5e9b
David <kirakosyandavit1995@gmail.com>
2025-07-29 16:52:37 +0300

    gateway-service

---
+++ category-srvice/src/main/java/am/armeniabank/categorysrvice/CategorySrviceApplication.java
@@ -0,0 +1,13 @@
+package am.armeniabank.categorysrvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorySrviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorySrviceApplication.class, args);
+	}
+
+}
